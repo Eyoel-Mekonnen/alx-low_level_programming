7629dfa1d569e96803cc6eb480b021fafc048e1b
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-05 03:00:09 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <Stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 if sucessful
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
