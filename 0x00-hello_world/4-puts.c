db9d5059bb29f96e47b11b78cc8f8191e56e0e69
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-05 02:52:54 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - starting point of the program.
+ *
+ * Return: 0 if succesful.
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
