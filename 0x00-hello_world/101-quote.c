2ccb1527aba08c24aa46dfa180c1218d91e8c4e5
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-05 05:31:38 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 1 if Successful
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora ");
+	fprintf(stderr, "Korpar, 2015-10-19\n");
+	return (1);
+}
