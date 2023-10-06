a487c8e791dec8034cf3a0a388da5a0d904540ca
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-06 16:32:01 +0000

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 if Sucessful
+ */
+int main(void)
+{
+	int num1 = 0, num2 = 1;
+
+	while (num1 < 9)
+	{
+		if (num1 < num2)
+		{
+			putchar(num1 + 48);
+			putchar(num2 + 48);
+			putchar(',');
+			putchar(' ');
+			num2++;
+			if (num2 > 9)
+			{
+				num1++;
+				num2 = 1;
+			}
+
+		}
+		if (num1 >= num2)
+			num2 = num2 + 1;
+	}
+	putchar('\n');
+	return (0);
+
+}
