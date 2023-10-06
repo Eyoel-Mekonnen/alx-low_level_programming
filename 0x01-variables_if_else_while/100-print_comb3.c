9b6ca9c8d7b20e403aec3e165766212f9a47b48a
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-06 19:22:05 +0000

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,16 @@ int main(void)
 
 	while (num1 < 9)
 	{
+
 		if (num1 < num2)
 		{
 			putchar(num1 + 48);
 			putchar(num2 + 48);
-			putchar(',');
-			putchar(' ');
+			if (num1 != 8 || num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}	
 			num2++;
 			if (num2 > 9)
 			{
