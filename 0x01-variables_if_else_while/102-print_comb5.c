d2340e351a53c7d5ed73fa5196999f057edc8766
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-09 08:28:30 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+void print_s(int num1, int num2, int num3, int num4);
+
+/**
+ * condition - condition 2
+ */
+void condition(void)
+{
+	putchar(',');
+	putchar(' ');
+}
 /**
  * main - main function
  *
@@ -8,26 +19,26 @@
 int main(void)
 {
 	int num1 = 0, num2 = 0, num3 = 0, num4 = 1;
-	while (num1 < 10)
+
+	while (num1 <= 9)
 	{
-		if ((num3 + num4) >= (num1 + num2))
-		{
-			putchar(num1 + 48);
-			putchar(num2 + 48);
-			putchar(' ');
-			putchar(num3 + 48);
-			putchar(num4 + 48);
-			putchar(' ');
+		if ((num1 == num3) && (num2 == num4))
 			num4++;
-		
+		if ((num1 <= num3) && (num3 + num4 >= num1 + num2))
+		{
+			print_s(num1, num2, num3, num4);
+			if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
+				break;
+			condition();
+			num4++;
 			if (num4 > 9)
 			{
-				num3++;
+				num3 = num3 + 1;
 				num4 = 0;
 			}
 			if (num3 > 9)
 			{
-				num2++;
+				num2 = num2 + 1;
 				num3 = 0;
 				num4 = 0;
 			}
@@ -39,8 +50,32 @@ int main(void)
 				num4 = 0;
 			}
 		}
-		
+		else if (num4 > 9 && num3 < 9)
+		{
+			num3++;
+			num4 = 0;
+		}
+		else
+			num4++;
 	}
 	putchar('\n');
 	return (0);
+}
+
+/**
+ * print_s - function that prints numbers
+ *
+ * @num1: number 1
+ * @num2: number 2
+ * @num3: number 3
+ * @num4: number 4
+ *
+ */
+void print_s(int num1, int num2, int num3, int num4)
+{
+	putchar(num1 + 48);
+	putchar(num2 + 48);
+	putchar(' ');
+	putchar(num3 + 48);
+	putchar(num4 + 48);
 }
