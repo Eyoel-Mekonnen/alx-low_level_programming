500625b569a580fa695ca18815814c187039ee9c
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-10 04:03:22 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
 
 /**
  * _isalpha - checks wether it is alpahbertic
- *
+ *@c: recievs the character
  * Return: 1 if alpahbet and 0 if not
  */
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
-	else 
+	else
 		return (0);
 }
