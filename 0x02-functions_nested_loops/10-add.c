d4aa61f269c09b0d5349f8c2688ab086aa904faf
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-10 07:47:02 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two number
+ * @num1: number one
+ * @num2: number two
+ *
+ * Return: the sum of the two numbers
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
