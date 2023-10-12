56ee6dee8107c80074e8f41ae7646c292d420ec4
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-12 03:53:24 -0700

    task 0

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,40 @@
  */
 int main(void)
 {
-	unsigned long int num1 = 1, num2 = 2, num3 = 0;
+	unsigned long int num1 = 1, num2 = 2, num3 = 0, num3b, num2b, num1b;
 	int i = 0;
 
 	while (i < 98)
 	{
 		if (i == 0)
 			printf("%lu, %lu, ", num1, num2);
-		num3 = num1 + num2;
+		if (i < 92)
+		{
+
+			num3 = num1 + num2;
+		}
 		if (i == 97)
 		{
 			printf("%lu", num3);
 			break;
 		}
-		printf("%lu, ", num3);
-		num1 = num2;
-		num2 = num3;
+		if (i < 90)
+		{
+			printf("%lu, ", num3);
+			num1 = num2;
+			num2 = num3;
+		}
+		if (i >= 90)
+		{
+
+			num1 = (num2 + num2b) / 10000000000;
+			num1b = (num2 + num2b) % 10000000000;
+			num2 = (num3 + num3b) / 10000000000;
+			num2b = (num3 + num3b) % 10000000000;
+			num3 = ((num1 + num1b) + (num2 + num2b)) / 10000000000;
+			num3b = ((num1 + num1b) + (num2 + num2b)) % 10000000000;
+			printf("%lu%lu, ", num3, num3b);
+		}
 		i++;
 	}
 	printf("\n");
