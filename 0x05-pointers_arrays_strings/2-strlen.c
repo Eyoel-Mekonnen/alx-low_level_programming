4d3271405559d7a4e2b39629848b4cd14d1a8d67
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-16 04:56:45 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns lenght of a string
+ * @s: accepts the string
+ *
+ * Return: lenght in integer
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+	
+}
