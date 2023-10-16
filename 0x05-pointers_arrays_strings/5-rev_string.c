14bff5de3f3a77170619de3ec936aefbf4b17bc4
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-16 08:57:28 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses the string
+ * @s: the string passed
+ *
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
