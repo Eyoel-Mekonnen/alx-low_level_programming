38df71a3e4e661d8e5074a1a1649050ffc20e5f4
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-16 12:15:02 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,13 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int len = 0;
 
-	while (str[i] != '\0')
+	while (str[len] != '\0')
+		len++;
+
+	while (i < (len - 1))
 	{
-		if (str[i] == 92)
-			break;
 		_putchar(str[i]);
 		i = i + 2;
 	}
