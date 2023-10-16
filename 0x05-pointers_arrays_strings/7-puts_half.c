ade9f8ce05300753117cdbe9222ec1d7b5eecb86
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-16 12:53:54 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,10 +9,15 @@ void puts_half(char *str)
 {
 	int len = 0;
 	int i;
+	int size;
 
 	while (str[len] != '\0')
 		len++;
-	for (i = len / 2; i < len; i++)
+	if (len % 2 != 0)
+		size = (len / 2) + 1;
+	else
+		size = len / 2;
+	for (i = size; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
