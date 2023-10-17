04bc1ee8279436b0663a3792f397a3c67d8fd083
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-17 03:48:01 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,19 +10,26 @@ int _atoi(char *s)
 {
 	int i = 0, sum = 0, num;
 	int indicator = 1;
+	int positive  = 0, negative = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] - '0' >= 0 && s[i] - '0' <= 9)
 		{
 				num = s[i] - '0';
-				if (s[i - 1] == '-')
-					indicator = -indicator;
 
 				sum = num + sum;
 				if (!(s[i + 1] - '0' >= 0 && s[i + 1] - '0' <= 9))
 					break;
 				sum = sum * 10;
 		}
+		else if (s[i] == '+')
+			positive++;
+		else if (s[i] == '-')
+			negative++;
+		if (negative % 2 == 0)
+			indicator = indicator;
+		else
+			indicator = -indicator;
 		i++;
 		
 	}
