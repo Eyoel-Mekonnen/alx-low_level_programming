0d8fa93f7b94ecd74ae7b02e252ab8aa73506f02
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-19 01:01:52 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len = 0;
-
-	while (src[len] != '\0')
-		len++;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	while (len < n)
-		dest[i] = '\0';
-
-	dest[i] = '\0';
+		for (; i < n; i++)
+			dest[i] = '\0';
 	return (dest);
 }
