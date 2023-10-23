480642cfd5596f3ef5c5148e106771cb427d0475
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-23 12:13:34 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string one
+ * @needle: second string
+ *
+ * Return: Adress of the substring in string one
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int len = 0, len2 = 0, i, j;
+
+	while (needle[len] != '\0')
+		len++;
+	while (haystack[len2] != '\0')
+		len2++;
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < len2; j++)
+		{
+			if (needle[i] == haystack[j])
+				return (&haystack[j]);
+		}
+	}
+	return (NULL);
+}
+
