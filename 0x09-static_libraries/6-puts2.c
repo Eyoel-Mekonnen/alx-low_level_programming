b00c3f28be71a1eebc6e0e8b15a62a07f457fb4d
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-27 13:10:22 -0700

    tasks

---
+++ 0x09-static_libraries/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: the string
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+int atoi_(char *s1)
+{
+	int len = 0, num1, sum = 0, i;
+
+	while (s1[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		if (s1[i] >= '0' && s1[i] <= '9')
+			num1 = s1[i] - '0';
+		sum = sum + num1;
+		if (s1[i + 1] == '\0')
+			break;
+		sum = sum * 10;
+	}
+	return (sum);
+}
+
+
+
+
+/**
+ * main - check the code
+ * @argc: number of arguments passed
+ * @argv: the argments passed as arrays
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+	int num1;
+	int num2;
+	
+	if (argc == 3)
+	{
+
+		num1 = atoi_(argv[1]);
+		num2 = atoi_(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}
