77291b4a8603df7e27b0b78ddf3a4e23889409be
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-30 09:40:05 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = (char *)malloc((sizeof(char) * size) + 1);
+	ptr = (char *)malloc((sizeof(char) * size));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
