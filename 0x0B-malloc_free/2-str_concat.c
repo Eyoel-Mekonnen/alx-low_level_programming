e6670d33f9ae73dbcbc79557ca01fd3592c83d76
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-30 10:52:53 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int len = 0, len2 = 0, i, j = 0;
 	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[len] != '\0')
 		len++;
 	while (s2[len2] != '\0')
