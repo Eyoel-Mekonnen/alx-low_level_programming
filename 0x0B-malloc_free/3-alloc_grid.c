504145b9bde46dba34395c723d5163290e7fc84b
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-10-31 05:03:27 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,8 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 	ptr = (int **)malloc(sizeof(int *) * height);
-
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		*(ptr + i) = (int *)malloc(sizeof(int) * width);
