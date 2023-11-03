4c00ef739e6924ed30ac1a7d6b018f2937db8398
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-11-03 06:20:56 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,55 +9,61 @@
  */
 char **strtow(char *str)
 {
-	int i = 0, count = 0, len = 0, wordcount = 0, setter = 0, j = 0;
+	int i = 0, count = 0, len = 0, wordcount, setter, j = 0,flag;
 	char **ptr;
 
 	while (str[len] != '\0')
 		len++;
-	while (i < len)
+	printf("%d\n", len);
+	while (i < len - 1)
 	{
-		if (str[i] == 32 || str[i] == '\0')
+		if (str[i] == ' ')
 			i++;
-		if (str[i] != 32 || str[i] != '\0')
+		else if (str[i] != ' ' || str[i] != '\0')
 		{
-			while (str[i] != 32 || str[i] != '\0')
+			while (str[i] != ' ' || str[i] != '\0')
 			{
-				i++;
-				if (str[i] == 32 || str[i] == '\0')
+				if (str[i] == ' ' || str[i] == '\0')
 				{
 					count++;
 					break;
 				}
+				else
+					i++;
 			}
 		}
+		
 	}
-	ptr = (char **)malloc(sizeof(char *) * count);
+	ptr = (char **)malloc(sizeof(char *) * count + 1);
 	if (ptr == NULL)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i < len - 1)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
+		if (str[i] == ' ')
 			i++;
-		if (str[i] != ' ' || str[i] == '\0')
+		else if (str[i] != ' ' || str[i] != '\0')
 		{
 			wordcount = 0;
-			setter = 0;
-			while ( str[i] != ' ' || str[i] != '\0')
+			while (str[i] != ' ' || str[i] != '\0')
 			{
 				wordcount++;
 				i++;
 				if (str[i] == ' ' || str[i] == '\0')
 					break;
 			}
-			*(ptr + j) = (char *)malloc(wordcount * (sizeof(char) + 1));
+			*(ptr + j) = (char *)malloc((wordcount + 1) * sizeof(char));
+			flag = i - wordcount;
+			setter = 0;
 			while (wordcount > 0)
 			{
-				(*(*(ptr + j) + setter)) = str[i - wordcount];
-				wordcount--;
-				setter++;
+				if (str[flag] == ' ' || str[flag] == '\0')
+					break;
+				(*(*(ptr + j) + setter)) = str[flag];
+				flag++;
+				++setter;
 			}
-			(*(*(ptr + j) + setter)) = '\0';
+			(*(*(ptr + j) + (setter))) = '\0';
 			j++;
 		}
 	}
