6b8263c6c984b7d9480acf153c3a19d32bbb857b
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-11-10 00:57:43 -0800

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	ptr = get_op_func(argv[2]);
-	if (ptr == NULL)
+	if (ptr == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(98);
