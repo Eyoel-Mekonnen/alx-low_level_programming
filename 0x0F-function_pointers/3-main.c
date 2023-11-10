3fa157bff26e3347b331e7ee14ba6d665a1a975c
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-11-10 00:42:00 -0800

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * main - takes in argment
+ * @argc: number of arguments
+ * @argv: the argument as a string
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, calculated;
+	int (*ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	ptr = get_op_func(argv[2]);
+	if (ptr == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	calculated = ptr(num1, num2);
+	printf("%d\n", calculated);
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+
+/**
+ * op_add - adds two numbers
+ * @a: first argument
+ * @b: Second argument
+ *
+ * Return: sum of two numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two numbers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two number
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: the mult of two number
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two numbers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: the division of two number
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo of two numbers
+ * @a: first argument
+ * @b: Second argument
+ *
+ * Return: Modulo of two numbers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
