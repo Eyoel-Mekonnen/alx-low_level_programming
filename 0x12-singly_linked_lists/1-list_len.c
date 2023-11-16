99e5d4a9fdcf9166dce8d314999e867143bf2cfd
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-11-16 06:50:22 -0800

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - return the number of elements in a lsit
+ * @h: the header passed
+ *
+ * Return: counts of number of element
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	while(h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
