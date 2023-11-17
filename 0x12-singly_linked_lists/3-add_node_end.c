63effd73624f43e1bb5e0f30b28f8cdc2a232c1e
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-11-16 23:46:52 -0800

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 #include <string.h>
 /**
- * add_node - adds a node at the last
+ * add_node_end - adds a node at the last
  * @head: address of pointer to a pointer
  * @str: the string passed
  *
- * Retrun: the added node address
+ * Return: the added node address
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *end_node;
 	list_t *tmp;
 	size_t counter = 0;
-	
+
 	end_node = malloc(sizeof(list_t));
 	if (end_node == NULL)
 		return (NULL);
