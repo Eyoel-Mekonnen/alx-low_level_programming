52ea804e66fd28e5134bbcd0cdf6cd0765715582
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-11-21 02:55:57 -0800

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,26 +9,29 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp, *ptr;
+	listint_t *tmp1, *tmp2, *ptr2;
 	unsigned int counter = 0;
-	
-	if (head == NULL)
+
+	ptr2 = (listint_t *)malloc(sizeof(listint_t));
+	if (ptr2 == NULL)
 		return (NULL);
-	tmp = *head;
-	ptr = (listint_t *)malloc(sizeof(listint_t));
-	if (ptr == NULL)
-		return (NULL);
-	ptr->n = n;
-	while (tmp != NULL)
+	ptr2->n = n;
+	if (*head == NULL)
+		return (ptr2);
+	tmp2 = *head;
+	tmp1 = tmp2;
+	while (tmp2 != NULL)
 	{
-		if (counter == idx)
-		{
-			ptr->next = tmp->next;
-			tmp->next = ptr;
-			return (ptr);
-		}
+		tmp2 = tmp2->next;
 		counter++;
-		tmp = tmp->next;
+		if (idx == counter)
+		{
+			tmp1->next = ptr2;
+			ptr2->next = tmp2;
+			return (ptr2);
+		}
+		else
+			tmp1 = tmp2;
 	}
 	return (NULL);
 }
