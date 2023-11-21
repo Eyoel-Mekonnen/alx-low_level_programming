436007efe678e09b3b63e97841eaeb21f8fabed1
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2023-11-21 03:18:45 -0800

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * size_t print_listint(const listint_t *h)
+ * print_listint - prints all elements
  * @h: address of first node
  *
  * Return: count of type of size_t
@@ -9,9 +9,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t counter = 0;
 	const listint_t *ptr;
-	
+
 	if (h == NULL)
-		return (-1);
+		return (counter);
 	ptr = h;
 	while (ptr != NULL)
 	{
