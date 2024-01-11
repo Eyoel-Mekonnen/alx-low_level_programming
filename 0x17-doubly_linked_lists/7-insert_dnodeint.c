0b953b8a8e41f071c53e11e89a82444f7adc85c0
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-11 16:24:20 +0300

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (ptr1->next->next == NULL)
 	{
 		*h = add_dnodeint_end(h, n);
-		free_dlistint(node);
+		free(node);
 		return (*h);
 	}
 	else
